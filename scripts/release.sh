0755c70e5ef00e06cda622201ed0bc5783dbfb8d
Dotun Jolaoso <dotunjolaosho@gmail.com>
2022-02-14 18:47:12 +0100

    fix

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,7 +3,7 @@ tag=$1
 : > ./cmd/VERSION && echo $tag >  ./cmd/VERSION
 
 # Commit version number & push
-git add VERSION
+git add ./cmd/VERSION
 git commit -m "Bump version to $tag"
 git push origin
 

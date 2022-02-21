910cb4f890357c416fd6fd27631f2fdbbe7d2e04
Dotun Jolaoso <dotunjolaosho@gmail.com>
2022-02-21 09:40:25 +0100

    add support for docker builds

--- convoy.go
+++ convoy.go
@@ -1,17 +1,1 @@
 package convoy
-
-import (
-	"embed"
-)
-
-//go:embed VERSION
-var f embed.FS
-
-func ReadVersion() ([]byte, error) {
-	data, err := f.ReadFile("VERSION")
-	if err != nil {
-		return nil, err
-	}
-
-	return data, nil
-}
